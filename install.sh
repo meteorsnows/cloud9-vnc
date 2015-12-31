8fa553ce2af4f47e103b07ca3bdfe6c68cdacc38
Jeremie Jarosh <jeremie@jarosh.org>
2015-12-31 21:57:46 +0000

    Add Custom Runner and Configuration via Installer

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ mkdir -p ${HOME}/bin
 cp run.sh ${HOME}/bin/run-x11.sh
 
 cp supervisord.conf ${HOME}/.supervisord.conf
+
+mkdir -p ${HOME}/workspace/.c9/runners
+cp runners/X11.run ${HOME}/workspace/.c9/runners/
+
+jsonpatch --indent 4 ${HOME}/workspace/.c9/project.settings runners/X11-run-config.jsonpatch > /tmp/project.settings
+mv /tmp/project.settings ${HOME}/workspace/.c9/project.settings
